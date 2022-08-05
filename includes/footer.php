e6dba03eb408c43af3f9c5be702ea0597f233782
Andrew Johnston <ajohnston@asu.edu>
2022-08-05 09:59:20 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
 
 			<div style="text-align:center;margin-bottom:15px"><a href="https://symbiota.org/" target="_blank">About Symbiota <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_symbiota.png" style="width:150px" /></a> <a href="https://biokic.github.io/symbiota-docs/" target="_blank">Symbiota help</a></div>
 
-			<p>
-				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:70px" /></a>
+			<p style="text-align:center">
+				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:50px" /></a>
 				<a href="http://idigbio.org" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:150px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
 			</p>
