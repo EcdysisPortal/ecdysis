1ab7ea22ae9d759a103177caaf2f854e5a683a65
Andrew Johnston <ajohnston@asu.edu>
2022-08-05 09:50:21 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 	<tr>
 		<td id="footer" colspan="3">
 
-			<div style="text-align:center;margin-bottom:15px"><a href="https://symbiota.org/" target="_blank">About Symbiota <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_symbiota.png" style="width:70px" /></a> <a href="https://biokic.github.io/symbiota-docs/" target="_blank">Guides and documentation</a></div>
+			<div style="text-align:center;margin-bottom:15px"><a href="https://symbiota.org/" target="_blank">About Symbiota <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_symbiota.png" style="width:150px" /></a> <a href="https://biokic.github.io/symbiota-docs/" target="_blank">Symbiota help</a></div>
 
 			<p>
 				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:70px" /></a>
