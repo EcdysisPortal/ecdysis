7cec5a2df0d3cfe0635de838cbcf8d12a681152b
Andrew Johnston <ajohnston@asu.edu>
2022-08-05 09:44:53 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,12 +2,14 @@
 	</tr>
 	<tr>
 		<td id="footer" colspan="3">
+
+			<div style="text-align:center;margin-bottom:15px"><a href="https://symbiota.org/" target="_blank">About Symbiota <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_symbiota.png" style="width:70px" /></a> <a href="https://biokic.github.io/symbiota-docs/" target="_blank">Guides and documentation</a></div>
+
 			<p>
 				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:70px" /></a>
 				<a href="http://idigbio.org" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:150px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
 			</p>
-			<div style="text-align:center;margin-bottom:15px">This project made possible by National Science Foundation Awards: <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=" target="_blank">#------</a></div>
 		</td>
 	</tr>
 </table>
