9c60c25154f7dab6e4788df7f393735b4d3fda29
Andrew Johnston <ajohnston@asu.edu>
2023-04-27 16:46:05 -0700

    index and css updates

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,18 @@
-    	</td>
-	</tr>
-	<tr>
-		<td id="footer" colspan="3">
-
-			<div style="text-align:center;margin-bottom:1px;"><a href="https://symbiota.org/" target="_blank">About Symbiota <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_symbiota.png" style="width:150px;vertical-align:middle;" /></a> <a href="https://biokic.github.io/symbiota-docs/" target="_blank">Symbiota help</a></div>
-
-			<div style="text-align:center;margin-bottom:15px">
-				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:250px" /></a>
-			</div>
-		</td>
-	</tr>
-</table>
+<footer>
+	<div class="logo-gallery">
+		<a href="https://www.nsf.gov" target="_blank">
+			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" />
+		</a>
+		<a href="http://idigbio.org" target="_blank" title="iDigBio">
+			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" />
+		</a>
+		<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center">
+			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" />
+		</a>
+		<a href="https://symbiota.org" target="_blank" title="Symbiota">
+			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_symbiota.png" />
+		</a>
+	</div>
+	<p>For more information about Symbiota, <a href="https://symbiota.org/docs" target="_blank" rel="noopener noreferrer">read the docs</a> or contact the <a href="https://symbiota.org/contact-the-support-hub/" target="_blank" rel="noopener noreferrer">Symbiota Support Hub</a>.</p>
+	</p>
+</footer>
