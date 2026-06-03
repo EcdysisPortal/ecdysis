48bb670be19d284eafaa52ff1ac06a351daebb8b
atticus29 <mark.aaron.fisher@gmail.com>
2026-06-03 12:05:21 -0700

    fix null accessor bug

--- includes/citationcollection.php
+++ includes/citationcollection.php
@@ -1,1 +1,4 @@
-	<?php echo $collData['collectionname']; ?>. Occurrence dataset (ID: <?php echo $collData['recordid'] ?>) <?php echo $collData['dwcaurl']; ?> accessed via the <?php echo ($DEFAULT_TITLE) ? $DEFAULT_TITLE : "Custom title for the portal"; ?>, <?php echo $SERVER_HOST . $CLIENT_ROOT; ?>, <?php echo date('Y-m-d'); ?>).
+<?php
+include_once('../config/symbini.php');
+?>
+<?php echo $collData['collectionname'] ?? ''; ?>. Occurrence dataset (ID: <?php echo $collData['recordid'] ?? '' ?>) <?php if (isset($collData['dwcaurl'])) echo $collData['dwcaurl']; ?> accessed via the <?php echo ($DEFAULT_TITLE) ? $DEFAULT_TITLE : "Custom title for the portal"; ?> Portal, <?php echo $SERVER_HOST . $CLIENT_ROOT; ?>, <?php echo date('Y-m-d'); ?>).
